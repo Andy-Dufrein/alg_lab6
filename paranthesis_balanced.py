f42de6acfbd93d59da08b20e068032c5d86f7453
Andy-Dufrein <sliza.nsk@gmail.com>
2019-04-24 04:12:27 +0700

    Comfortably numb

--- paranthesis_balanced.py
+++ paranthesis_balanced.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+from stack import Stack, StackOverflowError, StackIsEmptyError
+#Мне кажется, что это можно и более кратко задать, но я предполагаю, что так быстрее функция будет работать
 def is_paranthesis_balanced(s):
-    pass
-
+#    dic={"[":"]","{":"}","<":">","(":")"}
+    sko="{[<("
+    skc="}]>)"
+    dic={sko[a]: skc[a] for a in range(len(sko))}
+    if len(s)%2 != 0 or len(s)==0:
+        return False
+    else:
+        stack1=Stack(int(len(s)/2))
+    for char in s:
+        if char in sko:
+            stack1.push(char)
+        elif char in skc:
+            if len(stack1) == 0:
+                return False
+            else:
+                x=stack1.pop()  #Кажется, можно обойтись и без доп.переменной x
+                if char != dic.get(x):
+                    return False
+                else:
+                    continue
+    if len(stack1)==0: #На самом деле, это уже избыточное условие
+        return True
+    return False
--- stack.py
+++ stack.py
@@ -12,14 +12,41 @@ class StackIsEmptyError(RuntimeError):
 
 class Stack:
     def __init__(self, size):
-        self.storage = [0] * size
+        self.storage = [0]*size
         self.head = -1
+#        self.size = size-1
 
     def push(self, v):
-        pass
+        if self.head == (len(self.storage)-1):
+            raise StackOverflowError()
+        else:
+            self.head += 1
+            self.storage[self.head]=v
 
     def pop(self):
-        pass
+        if self.head == -1:
+            raise StackIsEmptyError()
+        else:
+            a=self.storage[self.head]
+            self.storage[self.head]=0
+            self.head -= 1
+            return a
 
     def __len__(self):
-        pass
+        return self.head + 1
+
+#stack1=Stack(5)
+#print(stack1.storage)
+#stack1.push("{")
+#stack1.push("{")
+#stack1.push("}")
+#stack1.push("{")
+#stack1.push("{")
+#print(len(stack1),stack1.head)
+#stack1.pop()
+#stack1.pop()
+#stack1.pop()
+#stack1.pop()
+#print(stack1.pop())
+#print(len(stack1),stack1.head)
+#print(stack1.storage)
